2ef962b3ba5c00a4d982eb203638194f53f244a6
Melvin Beemer <melvin.beemer@gmail.com>
2023-09-09 19:26:28 +0700

    Hello ZRC2 proxy

---
+++ README.md
@@ -0,0 +1,22 @@
+# DEX Core Protocol
+
+## Requirement
+`node@>=10`
+
+## Install dependencies
+`yarn`
+
+## Enter private keys for deployment
+`cp .env.sample .env`
+
+## Compile contracts
+`yarn compile`
+
+## Run tests
+`yarn test`
+
+## Deploy contracts
+```
+yarn deploy:testnet
+yarn deploy:mainnet
+```
---
+++ scripts/deploy.ts
@@ -0,0 +1,44 @@
+import { ethers, network, run } from "hardhat";
+
+const main = async () => {
+  // Compile contracts
+  await run("compile");
+  console.log("Compiled contracts.");
+
+  const networkName = network.name;
+
+  // Sanity checks
+  if (networkName === "mainnet") {
+    if (!process.env.KEY_MAINNET) {
+      throw new Error(
+        "Missing private key, refer to README 'Deployment' section"
+      );
+    }
+  } else if (networkName === "testnet") {
+    if (!process.env.KEY_TESTNET) {
+      throw new Error(
+        "Missing private key, refer to README 'Deployment' section"
+      );
+    }
+  }
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying to network:", networkName);
+
+  console.log("Deploying EIP20ZRC2Proxy..");
+  const plunderZrc2Proxy = await ethers.getContractFactory("EIP20ZRC2Proxy");
+  const zrc2Proxy = await plunderZrc2Proxy.deploy(
+    "0x18e1ad84cda23b28f33a7c47c0b672069f59733d"
+  );
+  await zrc2Proxy.deployed();
+
+  console.log("EIP20ZRC2Proxy deployed to:", zrc2Proxy.address);
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
